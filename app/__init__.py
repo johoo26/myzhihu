843c32051d4985bbc69646ceadcffacd1d2a4396
hsy <josh_shiyu@outlook.com>
2017-09-24 23:01:59 +0800

    sisth commit

--- app/__init__.py
+++ app/__init__.py
@@ -33,10 +33,10 @@ def create_app(config_name):
     login_manager.init_app(app)
     pagedown.init_app(app)
 
-    from main import main as main_blueprint
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from auth import auth as auth_blueprint
+    from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
 
